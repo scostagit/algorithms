c7339bbb866aa9de5ade1c20effbabf72c7ab9a2
Costa <sergio.costa@accenture.com>
2025-10-17 17:02:55 +0100

    fixed the find max average

--- patterns/04-slidingWindow/maxSlidingWindow.js
+++ patterns/04-slidingWindow/maxSlidingWindow.js
@@ -1,62 +1,60 @@
 /*
+239. Sliding Window Maximum
 
-
-643. Maximum Average Subarray I
-
-Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. 
-Any answer with a calculation error less than 10-5 will be accepted.
-
+You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the
+ array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+Return the max sliding window. 
 
 Example 1:
+Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+Output: [3,3,5,5,6,7]
+Explanation: 
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+Example 2: 
 
-Input: nums = [1,12,-5,-6,50,3], k = 4
-Output: 12.75000
-Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
-
-
-
-Example 2:
-
-Input: nums = [5], k = 1
-Output: 5.00000
- 
- [1,12,-5,-6,50,3]
+Input: nums = [1], k = 1
+Output: [1]
 
 Constraints:
 
-n == nums.length
-1 <= k <= n <= 105
+1 <= nums.length <= 105
 -104 <= nums[i] <= 104
-*/
+1 <= k <= nums.length
+ */
+
 
 /**
  * @param {number[]} nums
  * @param {number} k
- * @return {number}
+ * @return {number[]}
  */
+var maxSlidingWindow = function(nums, k) {
+   
+   if(nums.length === k) return nums[0];
 
-var findMaxAverage = function(nums, k) {
-    let windowSum = 0;
+    let maxNumber = 0;
+    let size = (nums.length - k);
 
-    // Initial window sum of first k elements
-    for (let i = 0; i < k; i++) {
-        windowSum += nums[i];
+    for(let i = 0; i <= size;  i++){
+
+        let window = nums.slice(i, (i+k));
+        var total = window.reduce((a,b)=> Math.max(a,b));
+        maxNumber = Math.max(total, maxNumber) ;  
+
+        //console.log(window,total);
     }
 
-    let maxSum = windowSum;
-
-    // Slide the window
-    for (let i = k; i < nums.length; i++) {
-        windowSum = windowSum - nums[i - k] + nums[i];
-        maxSum = Math.max(maxSum, windowSum);
-    }
-
-    // Return the maximum average
-    return maxSum / k;
+    return maxNumber;
+    
 };
-  
 
 
-console.log("Input: \narr:", [5] ,", \nk =1 \nOutput:", findMaxAverage( [5], 1), "\nExpected: ", 5);
-console.log("Input: \narr:", [0,1,1,3,3] ,", \nk =4 \nOutput:", findMaxAverage([0,1,1,3,3], 4), "\nExpected: ", 2);
-console.log("Input: \narr:",[1,12,-5,-6,50,3] ,", \nk =4 \nOutput:", findMaxAverage([1,12,-5,-6,50,3], 4), "\nExpected: ", 12.75);
+console.log("Input: \narr:", [1,3,-1,-3,5,3,6,7] , ", \nk =3 \nOutput:", maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3), "\nExpected: ", 7);
+console.log("Input: \narr:", [1] , ", \nk =1 \nOutput:", maxSlidingWindow([1], 1), "\nExpected: ", 1);
