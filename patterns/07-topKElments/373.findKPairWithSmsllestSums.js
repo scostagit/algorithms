0b253101fc3c3a19e3c59869228bb5f019715844
Costa <sergio.costa@accenture.com>
2025-11-12 12:36:27 +0000

    resolved 373. Find K Pair with smsllest Sums

--- patterns/07-topKElments/373.findKPairWithSmsllestSums.js
+++ patterns/07-topKElments/373.findKPairWithSmsllestSums.js
@@ -1,3 +1,91 @@
 /*
  373. Find K Pair with smsllest Sums
+
+ Medium
+Topics
+premium lock icon
+Companies
+You are given two integer arrays nums1 and nums2 sorted in non-decreasing order and an integer k.
+
+Define a pair (u, v) which consists of one element from the first array and one element from the second array.
+
+Return the k pairs (u1, v1), (u2, v2), ..., (uk, vk) with the smallest sums.
+ 
+
+Example 1:
+
+Input: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
+Output: [[1,2],[1,4],[1,6]]
+Explanation: The first 3 pairs are returned from the sequence: [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+
+
+Example 2:
+
+Input: nums1 = [1,1,2], nums2 = [1,2,3], k = 2
+Output: [[1,1],[1,1]]
+Explanation: The first 2 pairs are returned from the sequence: [1,1],[1,1],[1,2],[2,1],[1,2],[2,2],[1,3],[1,3],[2,3]
+ 
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 105
+-109 <= nums1[i], nums2[i] <= 109
+nums1 and nums2 both are sorted in non-decreasing order.
+1 <= k <= 104
+k <= nums1.length * nums2.length
 */
+
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number} k
+ * @return {number[][]}
+ */
+var kSmallestPairs = function(nums1, nums2, k) {
+
+   if(nums1.length === 0 || nums2.length === 0) return []; 
+
+   let heap = [];
+
+   for(let i = 0; i < Math.min(nums1.length, k); i++){
+      heap.push([nums1[i], nums2[0], 0]);
+   }
+  
+  let result =[];
+   
+  while(k > 0 && heap.length > 0){  
+
+    heap.sort((a, b) => (a[0] + a[1]) - (b[0] + b[1]));   
+
+    let  [num1, num2, idx2] = heap.shift(); 
+    result.push([num1, num2]);
+
+ 
+    if(idx2 +1 < nums2.length){
+      heap.push([num1, nums2[idx2+1], idx2+1]);
+    }    
+
+    k--;
+
+  }
+
+  return result;
+};
+
+
+console.log("373. Find K Pair with smsllest Sums\n");
+
+
+console.log("---------------------------------------------------------------------------------");
+console.log("Test Case 01  --");
+console.log("---------------------------------------------------------------------------------");
+console.log("Input: (", [1,7,11], [2,4,6], ", K: 3)\nOutput:", kSmallestPairs([1,7,11], [2,4,6],3),"\nExpected: ",[[1,2],[1,4],[1,6]]);
+console.log("---------------------------------------------------------------------------------");
+
+
+console.log("\n")
+console.log("Test Case 02  --");
+console.log("---------------------------------------------------------------------------------");
+console.log("Input: (",[1,1,2],[1,2,3],", K:2)\nOutput:",kSmallestPairs([1,1,2],[1,2,3], 2),"\nExpected: ", [[1,1],[1,1]]);
+console.log("---------------------------------------------------------------------------------");
--- patterns/07-topKElments/topKFrequent.js
+++ patterns/07-topKElments/topKFrequent.js
@@ -1,4 +1,4 @@
-function topKFrequent(nums, k) {
+/*function topKFrequent(nums, k) {
 
     let mapFrequeny = {};
 
@@ -14,6 +14,27 @@ function topKFrequent(nums, k) {
     let sorted = Object.entries(mapFrequeny).sort((a,b)=> b[1]-a[1]);
 
     return  sorted.slice(0,k).map((num)=> parseInt(num[0]));
+
+}
+
+// Test
+console.log(topKFrequent([1,1,1,2,2,3], 2)); // Output: [1, 2]
+
+*/
+
+
+
+function topKFrequent(nums, k) {
+
+   var frequency = { }
+
+   for(let i = 0; i < nums.length; i ++ ){    
+      frequency[nums[i]]? frequency[nums[i]]++: frequency[nums[i]] = 1;
+   }
+
+   let sorted = Object.keys(frequency).sort((a,b)=> b[1]-a[1]);
+
+   return sorted.map(n=> parseInt(n)).slice(0,k);
 
 }
 
